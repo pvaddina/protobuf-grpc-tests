861d546985857d7614f729761f5f679c0fb43aca
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2018-10-26 14:09:18 +0200

    Updates to the tests and correcting all hardcoded paths

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Protobuf Tests
 
 Several tests aimed to showcase or test usage of Protobuf. At the moment the focus is mainly for CMake integration and the different ways one can generate the messages from *.proto files. Please look at the tests themselves for details at what they actually do.
+
+Please note that these examples are also integrated with CMake and path to the Protobuf compiler and libraries are hardcoded. So it will not work out of the box. 
--- app-wo-findprotobuf/test.cpp
+++ app-wo-findprotobuf/test.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 
+#include "base.pb.h"
+#include "dht.pb.h"
+
+void BaseReader(const Base::BaseMsg& m)
+{
+  std::cout << "ID=" << m.id() << std::endl;
+  std::cout << "Name=" << m.name() << std::endl;
+}
+
+CDhtConfigCnf DhtReader(const CDhtConfigReq& m)
+{
+  std::cout << "ID=" << m.baseid.id() << std::endl;
+  std::cout << "Name=" << m.baseid.name() << std::endl;
+  std::cout << "State=" << m.state() << std::endl;
+  CDhtConfigCnf c;
+  c.set_result(Result::SUCCESS);
+  c.set_state(OnOff::OFF);
+  return c;
+}
+
+void BaseTester()
+{
+  Base::BaseMsg m;
+  m.set_name("This is a base msg");
+  BaseReader(m);
+}
+
+void DhtTester()
+{
+  CDhtConfigReq r;
+  r.baseid.set_name("CDhtConfigReq");
+  r.baseid.set_id(437);
+  r.set_state(OnOff::ON);
+  CDhtConfigCnf c = DhtReader(r);
+  std::cout << "Result: " << c.result() << ", state=" << c.state() << std::endl;
+}
+
 int main()
 {
-  std::cout << "Hello World !!!" << std::endl;
+  BaseTester();
   return 0;
 }
 
