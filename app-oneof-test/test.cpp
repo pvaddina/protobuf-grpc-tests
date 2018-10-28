3fa41e35a7dd6bc8c8546d1c530f60fa58670896
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2018-10-28 14:15:40 +0100

    Extensions to oneof test

--- app-oneof-test/test.cpp
+++ app-oneof-test/test.cpp
@@ -8,6 +8,37 @@ void Process(CHomeAutoParams& msg)
   {
     std::cout << "How far are the shades open = " << msg.shades().currentopenpercentage() << std::endl;
   }
+  else if (msg.has_lighting())
+  {
+    std::cout << "Living room=" << msg.lighting().livingroom() << ", Bedroom=" << msg.lighting().bedroom() << ", Kitchen=" << msg.lighting().kitchenroom() << std::endl;
+  }
+  else if (msg.has_heating())
+  {
+    std::cout << "Name=" << msg.heating().name() << ", CurrentTemp=" << msg.heating().currenttemp() << ", DesiredTemp=" << msg.heating().desiredtemp() << std::endl;
+  }
+}
+
+void SetShades(CHomeAutoParams& m)
+{
+  CShadesCtrl* sc = m.mutable_shades();
+  sc->set_state(OpenUpto::OPEN);
+  sc->set_currentopenpercentage(34);
+}
+
+void SetLighting(CHomeAutoParams& m)
+{
+  CLightingCtrl* lc = m.mutable_lighting();
+  lc->set_livingroom(LightOnOff::ON);
+  lc->set_bedroom(LightOnOff::ON);
+  lc->set_kitchenroom(LightOnOff::OFF);
+}
+
+void SetHeating(CHomeAutoParams& m)
+{
+  CHeatingCtrl* hc = m.mutable_heating();
+  hc->set_name("Automated heating control");
+  hc->set_currenttemp(32.445);
+  hc->set_desiredtemp(55.32);
 }
 
 void BaseTester()
@@ -16,10 +47,13 @@ void BaseTester()
 
   CHomeAutoParams m;
 
-  CShadesCtrl* sc = m.mutable_shades();
-  sc->set_state(OpenUpto::OPEN);
-  sc->set_currentopenpercentage(34);
+  SetShades(m);
+  Process(m);
 
+  SetHeating(m);
+  Process(m);
+
+  SetLighting(m);
   Process(m);
 }
 
